b38703c648ecfc6b50a671fe90bec1a06af60b4a
Michael Sturm <59833741+MichaelCSturm@users.noreply.github.com>
2024-01-27 11:12:33 -0600

    Add files via upload

---
+++ SturmMichaelM2/Program1/Program1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//a-(n* int(a/n)) is the key to finding out the mod of a/n!
+
+Console.WriteLine("What is the number you're trying to find the mod for!");
+
+int num = Convert.ToInt32(Console.ReadLine());
+bool primeTime = true;
+for (int i = 2; i< num; i+=1)
+{
+    for (int newNum = 2; newNum < i; newNum += 1)
+    {
+        primeTime = true;
+
+        if (i % newNum == 0)
+        {
+
+            primeTime = false;
+            break;
+        }
+
+        
+    }
+    if (primeTime== true){
+        Console.WriteLine(i); }
+}
+// This was really fun!
---
+++ SturmMichaelM2/Project2/Project2/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] numbers = [5, 2, 3, 1, 4, 200, 90, 100000, 10000000, 23, 1,1,1,1,1,];
+int temp = 0;
+var watch = System.Diagnostics.Stopwatch.StartNew();
+for (int i = 0; i< numbers.Length; i++) {
+    //Console.WriteLine(numbers[i]);
+    for (int j = 0; j < numbers.Length; j++)
+    {
+        
+        temp = numbers[i];
+        if (numbers[j] > numbers[i])
+        {
+            numbers[i] = numbers[j];
+            numbers[j] = temp;  
+        }
+        
+
+
+
+    }
+
+
+}
+foreach(int num in numbers)
+{
+
+    Console.WriteLine(num);
+}
+watch.Stop();
+Console.WriteLine($"Using system.dia whatever to calcuate this operation took ${watch} to work");
+Console.WriteLine("You sent me down a rabithole of looking into cool algorythms. If i had more time I woulda tried the heap one!");
